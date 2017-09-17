b1d94ca3e04fe78466aa860589d00fc2f5dd8f56
neerajkumar0 <emailneer@gmail.com>
2017-09-16 21:56:30 -0500

    Test python program

---
+++ TrialPythonProgram/temp.py
@@ -0,0 +1,16 @@
+class family:
+	'details of a family'
+	communityCount =0
+
+	def __init__(self, familyName, noOfMember):
+		self.familyName = familyName
+		self.noOfMember = noOfMember
+		communityCount += 1
+		count =0
+		while(count < noOfMember)
+			str = raw_input("Enter your input: ");
+
+	def DisplayFamilyInfo(self)
+		print ("Family Name :", self.familyName);
+		print ("Family Member Count : ", self.noOfMember);
+
--- TrialPythonProgram/test.py
+++ TrialPythonProgram/test.py
@@ -1,3 +1,55 @@
 #!/usr/bin/python
 
 print ("Hello, Python!");
+
+class familyMember:
+	'Details of family member'
+
+	def __init__(self,name,age,sex,memberType):
+		self.name = name
+		self.age = age
+		self.sex = sex
+		self.memberType = memberType
+
+	def DisplayMember(self):
+		print ("\nName : ", self.name, " Age : ", self.age, " Sex : ", self.sex , "\n\n");
+
+
+#member1 = familyMember("Neeraj Kumar", 40, 'M','F')
+#member2 = familyMember("Sandhya Kumari", 39, 'F','M')
+#member3 = familyMember("Rishabh Anand", 11, 'M','C')
+#member4 = familyMember("Tejas Anand", 6, 'M','C')
+
+
+class family:
+	'details of a family'
+	familyCount =0
+
+	def __init__(self, familyName, noOfMember):
+		self.familyName = familyName
+		self.noOfMember = noOfMember
+		self.familyMembers = []
+#		familyCount += 1
+		count =0
+		while(count < self.noOfMember):
+			count += 1
+			name = input("\nEnter Name:")
+			age = input("\n age:")
+			sex = input("\n sex:")
+			memberType = input("\n member Type:")
+			member = familyMember(name,age,sex,memberType)
+			self.familyMembers.append(member)
+
+	def DisplayFamily(self):
+		print ("\n This is the ", self.familyName, " family");
+		print ("\n the family members are:");
+		count =0
+		while(count < self.noOfMember):
+			self.familyMembers[count].DisplayMember()
+			count += 1
+
+
+family1 = family("Kumar Family", 4)
+family1.DisplayFamily()
+
+input("\n\nPress the enter key to exit.");
